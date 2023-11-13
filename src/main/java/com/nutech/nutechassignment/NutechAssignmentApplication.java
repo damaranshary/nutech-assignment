f5f8d3b38abf57dc42005ae3d57db510ec721adf
Damar Anshary <damar.galihanshary7@gmail.com>
2023-11-13 19:02:58 +0700

    initial commit

---
+++ src/main/java/com/nutech/nutechassignment/NutechAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.nutech.nutechassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutechAssignmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NutechAssignmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nutech/nutechassignment/NutechAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nutech.nutechassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NutechAssignmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
